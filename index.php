91ecd6e1f475a352bbb49bc64016e336dab898a4
Diosmar Arbey Cardenas Duran <developer@MacBook-Pro-de-Diosmar.local>
2022-03-25 21:28:02 -0500

    Eliminamos la posicion vacia inicial y vacia final del array

--- index.php
+++ index.php
@@ -3,8 +3,20 @@
 $RequestUrl = parse_url($_SERVER["REQUEST_URI"]);
 $url = $RequestUrl["path"];
 //var_dump($RequestUrl); # Sacamos la posicion path de la url
-//Covertimos la url en un arreglo
+
+//Covertimos la la informacion de la url en un arreglo y guardamos los parametros en $url
 $url = explode("/", $url);
+
+
+//eliminamos la posicion vacia inial y vacia final
+$lastArrayPosition = count($url) -1;
+
+if(empty($url[$lastArrayPosition])){
+    array_shift($url); //Esta funcion elimina la primer posicion vacia del array $url
+    array_pop($url); //Esta funcion elimina la ultima posicion vacia del array $url
+}else{
+    array_shift($url);
+} 
 var_dump($url);
 
 ?>
